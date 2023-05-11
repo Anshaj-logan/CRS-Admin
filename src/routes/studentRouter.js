9492a858ec7910f36a28fa259756ae9399ecdadd
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-05-11 18:20:31 +0530

    dash

--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -11,6 +11,8 @@ studentRouter.get('/student',  async function (req, res) {
   const data=await registermodel.find()
   res.render('mng_std',{data})
   })
+
+  
   studentRouter.get('/delete/:name',  async function (req, res) {
     const id=req.params.name
     try {
