4ecc26c3b6644c3136fa32d4b50120ea98eb49e5
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-05-11 20:06:51 +0530

    change

--- src/routes/companyRouter.js
+++ src/routes/companyRouter.js
@@ -1,15 +1,46 @@
 const express = require('express')
 const companyRouter = express.Router()
+const login = require('../models/login')
+const company = require('../models/Cmpreg')
 companyRouter.use(express.static('./public'))
 
 
 
 
-companyRouter.get('/company', function (req, res) {
-    res.render('mng_cmpny')
-  })
-
-
+companyRouter.get('/company', async function (req, res) {
+  try {
+    const data = await  login.aggregate([
+      {
+          '$lookup': {
+            'from': 'cmpregister_tbs', 
+            'localField': '_id', 
+            'foreignField': 'login_id', 
+            'as': 'data'
+          }
+        },
+      {
+        "$unwind": "$data"
+      },
+      {
+        "$group": {
+          "_id": "$_id",
+          "user_id": { "$first": "$data._id" },
+          "company_name": { "$first": "$data.company_name" },
+          "email": { "$first": "$data.email" },
+          "phone": { "$first": "$data.contact" },
+          "status": { "$first": "$status" },
+        }
+      }
+  
+    ])
+      // res.render('mng_cmpny')
+      res.json({data})
+   
+  
+  } catch (error) {
+    
+  }
+})
 
 
 
--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -1,31 +1,71 @@
 const express = require('express')
-const stdmodel = require('../models/stdnt')
-// const stdmodel = require('.  ./models/stdnt')
+const student = require('../models/register')
+const login = require('../models/login')
 const studentRouter = express.Router()
 studentRouter.use(express.static('./public'))
 
 
 
 
-studentRouter.get('/student',  async function (req, res) {
-  const data=await registermodel.find()
-  res.render('mng_std',{data})
-  })
-
-  
-  studentRouter.get('/delete/:name',  async function (req, res) {
-    const id=req.params.name
-    try {
-      const delete_data=await addntfcnmodel.deleteOne({_id:id})
-      if (delete_data.deletedCount==1)
-      {
-        res.redirect("/student/student")
+studentRouter.get('/view_student',  async function (req, res) {
+  const data = await  login.aggregate([
+    {
+        '$lookup': {
+          'from': 'register_tbs', 
+          'localField': '_id', 
+          'foreignField': 'login_id', 
+          'as': 'data'
+        }
+      },
+    {
+      "$unwind": "$data"
+    },
+    {
+      "$group": {
+        "_id": "$_id",
+        "user_id": { "$first": "$data._id" },
+        "name": { "$first": "$data.name" },
+        "email": { "$first": "$data.email" },
+        "phone": { "$first": "$data.contact" },
+        "status": { "$first": "$status" },
       }
-    } catch (error) {
+    }
+
+  ])
+  // res.json({data})
+  res.render('mng_std',{data})
+})
+
+
+studentRouter.get('/delete/:id',  async function (req, res) {
+  const id=req.params.id
+  try {
+    const delete_data=await student.deleteOne({login_id:id})
+    if (delete_data.deletedCount===1)
+    {
+      const delete_data1=await login.deleteOne({_id:id})
+      if(delete_data1.deletedCount===1){
+        res.redirect("/student/view_student")
+      }
       
     }
+  } catch (error) {
     
+  }
+  
+})
+
+
+studentRouter.get("/approve/:id", async (req, res) => {
+  const id = req.params.id
+  console.log(id);
+  login.updateOne({ _id: id }, { $set: { status: "1" } }).then((details) => {
+      
+      res.redirect('/student/view_student')
   })
+
+});
+
 
 
 
